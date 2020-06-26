61836dd46efd6e0491232ad1d91baff5cae64358
BijuAle <bijuale@hotmail.com>
2020-06-26 13:50:26 +0545

    add global styles

---
+++ gatsby-browser.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import Theme from "./src/themes/theme"
+
+const GlobalStyles = createGlobalStyle`
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-size;
+}
+body,html{
+  color:${props => props.theme.colors.lightText};
+  font-family:${props => props.theme.fonts.main};
+  height:100%;
+  background-color: ${props => props.theme.colors.darkBg};
+}
+`
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    {element}
+  </ThemeProvider>
+)
---
+++ src/themes/theme.js
@@ -0,0 +1,10 @@
+export default {
+  fonts: {
+    main: "EB Garamond",
+    code: "Roboto Mono, monospace",
+  },
+  colors: {
+    darkBg: "#1a202c",
+    lightText: "gray",
+  },
+}
