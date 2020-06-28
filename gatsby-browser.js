bc0e2a09712f8f7745fd01623f34069fb8e171bc
BijuAle <bijuale@hotmail.com>
2020-06-28 15:21:56 +0545

    add container element

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,7 @@ const GlobalStyles = createGlobalStyle`
 *{
   margin:0;
   padding:0;
-  box-sizing: border-size;
+  box-sizing: border-box;
 }
 body,html{
   color:${props => props.theme.colors.lightText};
