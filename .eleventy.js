aaf707684286e1d1a115df6e48e046d7fa9fe9bb
Biju Ale <bijuale@Bijus-MacBook-Pro.local>
2024-10-19 21:20:17 +0545

    Improved Parser & Slug

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,30 @@
 const { DateTime } = require("luxon");
+const slugify = require("slugify");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addFilter("formatted", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-});
-    return {
-      markdownTemplateEngine: "njk",
-      dir: {
-        input: "src",
-        output: "dist",
-        data:"_data"
-      }
-    };
+  });
+
+  eleventyConfig.addFilter("slug", (str) => {
+    if (!str) {
+      return;
+    }
+
+    return slugify(str, {
+      lower: true,
+      strict: true,
+      remove: /[']/g,
+    });
+  });
+
+  return {
+    markdownTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+      data: "_data",
+    },
   };
+};
